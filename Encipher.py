461c41c91df66f6a617f4ad6d4ee13aab9b8c622
Daniel Antunez Manra <manra_04@hotmail.com>
2018-10-10 22:27:55 -0500

    Creando proyecto BabyDES

---
+++ Encipher.py
@@ -0,0 +1,5 @@
+#   En esta ocacion trato de hacer un programa que cifre       #
+#   deacuerdo con el algoritmo DES                             #
+#                                                              #
+#                                                              #
+################################################################
