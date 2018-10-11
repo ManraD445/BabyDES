8182acac658479d1df80056f78b51adbd1188f85
Daniel Antunez Manra <manra_04@hotmail.com>
2018-10-11 00:08:03 -0500

    Se divide el texto plano en bloques de 64bits

--- Encipher.py
+++ Encipher.py
@@ -1,5 +1,22 @@
+################################################################
+#                                                              #
 #   En esta ocacion trato de hacer un programa que cifre       #
 #   deacuerdo con el algoritmo DES                             #
 #                                                              #
-#                                                              #
 ################################################################
+
+
+Mensaje = "Hola es un gusto saber de ti"
+
+bloques=[] #Creo una lista para guardar los bloques de 64 bits.
+
+
+x=8
+y=0
+
+for i in range(4): 
+    bloques.append(Mensaje[y:x])
+    x=x+8
+    y=y+8
+
+print(bloques)
